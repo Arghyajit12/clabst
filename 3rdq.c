8947b2b411f188fc8433eb9c5a7c814a3e66dec3
Arghyajit <arghyajitsaha69@gmail.com>
2024-03-23 19:58:49 +0530

    size of

---
+++ 3rdq.c
@@ -0,0 +1,13 @@
+// using size of
+#include<stdio.h>
+int main(){
+   int intType;
+   float floatType;
+   double doubleType;
+   char charType;
+   printf("size of int :%dbytes\n",sizeof(intType));
+    printf("size of float :%dbytes\n",sizeof(floatType));
+    printf("size of double :%dbytes\n",sizeof(doubleType));
+    printf("size of char :%dbytes\n",sizeof(charType));
+    return 0;
+}
