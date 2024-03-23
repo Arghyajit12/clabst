0ccdf89f44ecaada50891a7431b6e39eb52fc9c4
Arghyajit <arghyajitsaha69@gmail.com>
2024-03-23 20:39:57 +0530

    area of circle

---
+++ 8thq.c
@@ -0,0 +1,10 @@
+// area of circle
+#include<stdio.h>
+int main(){
+    float area , radius;
+    printf("enter radius of circle:");
+    scanf("%f%f",&radius,&radius);
+    area = 3.14*radius*radius;
+    printf("area of circle: %.2f",area);
+    return 0;
+}
