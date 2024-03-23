2bd082d36d02454f79dd0bf86592a2c770c3d9ba
Arghyajit <arghyajitsaha69@gmail.com>
2024-03-23 21:57:18 +0530

    fibonacci series

---
+++ 17thq.c
@@ -0,0 +1,13 @@
+//fibonacci series
+#include<stdio.h>
+int main(){
+    int i=1,j=0,n;
+    printf("enter number");
+    scanf("%d",&n);
+    while(j<=n){
+        printf("%d",j);
+        i=i+j;
+        j=i-j;
+    }
+    return 0;
+}
