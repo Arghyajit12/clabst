047e434a2d006fa3f9b19775bcae317b8c0c8c6a
Arghyajit <arghyajitsaha69@gmail.com>
2024-03-23 20:10:22 +0530

    odd or even

---
+++ 5thq.c
@@ -0,0 +1,12 @@
+// odd or even
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter ur number");
+    scanf("%d",&n);
+    if (n%2==0)
+    printf("even number");
+    else 
+    printf("odd  number");
+    return 0;
+}
