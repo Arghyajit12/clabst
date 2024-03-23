9a4941649804fc8f8f94386d2da3fedc11dbc37e
Arghyajit <arghyajitsaha69@gmail.com>
2024-03-23 22:03:14 +0530

    prime no. or not

--- 18thq.c
+++ 18thq.c
@@ -1,1 +1,21 @@
-//
+//prime number or not 
+#include<stdio.h>
+int main(){
+    int n,i ;
+    printf("enter number");
+    scanf("%d",&n);
+    int a =0;
+    for(i=2;i<=n-1;i++)
+    if(n%i==0){
+     a=1;
+     break;
+    }
+    if (n==1)
+    printf("1 is neither prime nor composite");
+      else if (a==0)
+    printf("given number is prime");
+    else
+    printf("given number is composite");
+    return 0;
+}
+ 
