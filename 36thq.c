45021f1ba828c6c4a381c2a34e74d80831e032ef
Arghyajit <arghyajitsaha69@gmail.com>
2024-03-24 16:10:52 +0530

    Diamond pattern

--- 36thq.c
+++ 36thq.c
@@ -1,23 +1,23 @@
 // diamond pattern
   #include<stdio.h>
   int main(){
-    int a,b,c,n;
+    int i,j,k,n;
     printf("enter the number of rows:");
     scanf("%d",&n);
-    for(a=1;a<=n;a++){
-        for(b=n;b>a;b--){
+    for(i=1;i<=n;i++){
+        for(j=n;j>i;j--){
             printf(" ");
         }
-        for(c=1;c<=(2*a)-1;c++){
+        for(k=1;k<=(2*i)-1;k++){
             printf("*");
         }
         printf("\n");
     }
-     for(a=1;a<=n;a++){
-        for(b=1;b<=a;b++){
+     for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
             printf(" ");
         }
-        for(c=1;c<=2*(n-a)-1;c++){
+        for(k=1;k<=2*(n-i)-1;k++){
             printf("*");
         }
         printf("\n");
