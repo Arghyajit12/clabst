05a42cdcb7aa584486a5dcb814cf3a90685e2a5f
Arghyajit <arghyajitsaha69@gmail.com>
2024-03-24 13:06:37 +0530

    left half pyramid

---
+++ 31stq.c
@@ -0,0 +1,17 @@
+// left half pyramid pattern
+#include<stdio.h>
+int main(){
+    int i,j,k,n;
+    printf("enter number of rows:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=0;j<2*(n-i)-1;j++){
+            printf(" ");
+        }
+        for(k=0;k<=i;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
